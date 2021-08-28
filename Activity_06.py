e6bdd829f8215610cb25816e689f135b26de1636
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-28 16:40:06 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -7,15 +7,12 @@ numbers = input_line.split(' ')
 '''creating sliced list'''
 sliced_list = numbers[0:3]
 
-'''length of list'''
-n = len(numbers)
-  
 print(f'sliced list = {sliced_list}')
 
 '''replacement of first and last elements with 0'''
-rep_list_1 = numbers
+rep_list_1 = numbers[:-1]
 rep_list_1[0] = 0
-rep_list_1[n-1] = 0
+rep_list_1.append(0)
 
 rep_list_2 = sliced_list
 rep_list_2[0] = 0
