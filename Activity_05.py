4a48c4b5bbfa16f706077c674dfbb40441ae6c17
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-28 14:20:38 +0530

    Update Activity_05.py

--- Activity_05.py
+++ Activity_05.py
@@ -1,6 +1,7 @@
+print("input numbers")
 input_line = input()
 number_list = input_line.split(' ')
 sum = 0
 for i in number_list:
     sum = sum + int(i)
-print(sum)
+print("Sum of all the numbers is = %d" %(sum))
