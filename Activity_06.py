b2cc53166ec580786ec90a0fe3d8540936daf817
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-28 16:08:29 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -2,6 +2,9 @@ input_line = input("input numbers: ")
 numbers = input_line.split(' ')
 sliced_list = numbers[0:3]
 
+for i in range(3):
+    sliced_list[i] = int(sliced_list[i])
+  
 print(f'sliced list = {sliced_list}')
 
 rep_list_1 = numbers
@@ -12,15 +15,11 @@ rep_list_2 = sliced_list
 rep_list_2[0] = 0
 rep_list_2[2] = 0
 
-i = 0
 for i in range(5):
     rep_list_1[i] = int(rep_list_1[i])
-    i += 1
 
-i = 0
 for i in range(3):
     rep_list_2[i] = int(rep_list_2[i])
-    i += 1
 
 print(f'replaced list-1 = {rep_list_1}')
 print(f'replaced list-2 = {rep_list_2}')
