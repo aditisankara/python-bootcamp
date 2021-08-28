3de9d0d4bdacb81b1437ba3fe29fb7b4a083e5e6
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-28 13:46:16 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,3 +1,10 @@
 a = int(input("a = "))
 b = int(input("b = "))
 print("%d + %d = %d" %(a, b, (a+b)))
+
+a = int(input("a = "))
+b = int(input("b = "))
+c = a + b
+s = f'{a} + {b} = {c}'
+print(s)
+
