6eefed942ffedb8001a6b751f026d4b7c27d56d4
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-28 16:23:58 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -1,25 +1,25 @@
+'''prompt for input'''
 input_line = input("input numbers: ")
+
+'''creating a list from the input'''
 numbers = input_line.split(' ')
+
+'''creating sliced list'''
 sliced_list = numbers[0:3]
 
-for i in range(3):
-    sliced_list[i] = int(sliced_list[i])
+'''length of list'''
+n = len(numbers)
   
 print(f'sliced list = {sliced_list}')
 
+'''replacement of first and last elements with 0'''
 rep_list_1 = numbers
 rep_list_1[0] = 0
-rep_list_1[4] = 0
+rep_list_1[n-1] = 0
 
 rep_list_2 = sliced_list
 rep_list_2[0] = 0
 rep_list_2[2] = 0
-
-for i in range(5):
-    rep_list_1[i] = int(rep_list_1[i])
-
-for i in range(3):
-    rep_list_2[i] = int(rep_list_2[i])
 
 print(f'replaced list-1 = {rep_list_1}')
 print(f'replaced list-2 = {rep_list_2}')
