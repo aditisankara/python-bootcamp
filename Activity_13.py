7ed270c48ae752c3035709faefe6baaa717aa2f1
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-29 15:56:35 +0530

    Update Activity_13.py

--- Activity_13.py
+++ Activity_13.py
@@ -1,8 +1,20 @@
-n = int(input("enter the start number: "))
-m = int(input("enter the end number: "))
+def ip_range():
+    #to input start and end of range
+    a = int(input("enter the start number: "))
+    b = int(input("enter the end number: "))
+    return a, b
 
-i = n
-while i <= m:
-    if i % 2 != 0:
-        print(i)
-    i += 1
+def disp_odd_numbers(a, b):
+    #to display odd numbers between a and b
+    odd_numbers = [*range(a, b+1, 2)]
+    print(*odd_numbers, sep = "\n")
+
+def main():
+    n, m = ip_range()
+
+    if n % 2 == 0:
+        n = n + 1
+
+    disp_odd_numbers(n, m)
+
+main()
