64ce0312726042e6f4da8f2d8cc290c8a0564fa7
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-29 08:58:00 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -8,12 +8,11 @@ k = l**2 + b**2 + h**2
 '''finding volume'''
 vol = (b**2) * (h**2) / (k**0.5)
 
-print("volume = ", end = "")
-print('%.3f' %vol)
+print("volume = %.3f" %vol)
 
 '''finding radius'''
 r = (3*vol/(4*3.142)) ** (1/3)
 
-print("radius = ", end = "")
-print('%.3f' %r)
+print("radius = %.3f" %r)
+
 
