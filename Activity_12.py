84e819e8b244fd38e9b924d5069cdc5f8701919b
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-29 11:17:59 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -1,14 +1,27 @@
-input_line = input("enter three numbers: ")
-n = input_line.split(' ')
+def ip_numbers():
+    #to input numbers
+    ip = input("input numbers: ")
+    return ip
 
-#comparing the three numbers
-if n[0] >= n[1] and n[0] >= n[2] :
-    l = n[0]
-    
-elif n[1] >= n[0] and n[1] >= n[2] :
-    l = n[1]
-    
-else :
-    l = n[2]
+def greatest(a, b, c):
+    #to compare three values
+    if a >= b and a >= c:
+        l = a
+    elif b >= a and b >= c:
+        l = b
+    else:
+        l = c
+    return l
 
-print(l, "is the greatest among ", n[0], ",", n[1], "and", n[2])
+def display(l, a, b, c):
+    #to display greatest of three numbers
+    print(l, "is the greatest number among ", a, ",", b, "and", c)
+
+def main():
+    ip_line = ip_numbers()
+    n = ip_line.split(' ')
+
+    g = greatest(n[0], n[1], n[2])
+    display(g, n[0], n[1], n[2])
+
+main()
