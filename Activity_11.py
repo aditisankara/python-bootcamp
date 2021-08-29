884f8fa9ca9372656363fd0a73a68b73550e7070
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-29 10:18:30 +0530

    Update Activity_11.py

--- Activity_11.py
+++ Activity_11.py
@@ -1,22 +1,23 @@
-def input_numbers():
-    '''to input two numbers'''
-    x = int(input("first number = "))
-    y = int(input("second number = "))
-    return x, y
+def input_number():
+    #to input a number
+    x = int(input("enter number: "))
+    return x
 
 def add(x, y):
-    '''addition of two numbers'''
+    #addition of two numbers
     sum = x + y
     return sum
 
 def display(x, y, sum):
-    '''to display the addition of two numbers'''
+    #to display the addition of two numbers
     print(x, " + ", y, " = ", sum)
     
 def main():
-    a, b = input_numbers()
+    a = input_number()
+    b = input_number()
     summation = add(a, b)
     display(a, b, summation)
-
+    
+    
 main()
 
