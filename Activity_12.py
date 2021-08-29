8c90f1d0d3e32b0fec2100e33e8c75b49bc6485f
aditisankara <76278142+aditisankara@users.noreply.github.com>
2021-08-29 11:33:37 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -3,6 +3,10 @@ def ip_numbers():
     ip = input("input numbers: ")
     return ip
 
+def to_integer(x):
+    x = int(x)
+    return x
+
 def greatest(a, b, c):
     #to compare three values
     if a >= b and a >= c:
@@ -19,7 +23,9 @@ def display(l, a, b, c):
 
 def main():
     ip_line = ip_numbers()
-    n = ip_line.split(' ')
+    numbers = ip_line.split(' ')
+    integers = map(to_integer, numbers)
+    n = list(integers)
 
     g = greatest(n[0], n[1], n[2])
     display(g, n[0], n[1], n[2])
